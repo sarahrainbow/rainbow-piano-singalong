5463769f8a5ec68b90bd25b26db87b7d2cc3bd29
Sarah Rainbow <sarah.rainbow@bbc.co.uk>
2020-01-24 15:57:27 +0000

    style tweaks

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -3,12 +3,31 @@ import Quote from '../Quote';
 import { Element } from 'react-scroll';
 import 'react-piano/dist/styles.css';
 import MyPiano from '../MyPiano';
+import Switch from "react-switch";
 
 class Home extends Component {
+    constructor() {
+        super();
+        this.state = { checked: false };
+        this.handleChange = this.handleChange.bind(this);
+      }
+
+      handleChange(checked) {
+        this.setState({ checked });
+      }
+
+      toggleMusicalTyping() {
+
+      }
+
     render(){
         return(
             <Element name='home' id="Home" className="container-fluid section">
                 <MyPiano />
+                <label id="musicalTypingSwitch">
+                    <Switch onChange={this.handleChange} checked={this.state.checked} />
+                    <p>Musical typing</p>
+                </label>
                 <Quote className="center">Best night of my life</Quote>    
             </Element>
         );
