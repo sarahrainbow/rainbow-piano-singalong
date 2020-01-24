0e0438d810cd81f97737549b49075b67c424df43
Sarah Rainbow <sarah.rainbow@bbc.co.uk>
2020-01-24 10:37:36 +0000

    Updates

---
+++ precache-manifest.37c8ed738c0734a07d9a4c275cdd08c8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a9dcae54ea4c80ab5fce579429bc856",
+    "url": "/rainbow-piano-singalong/index.html"
+  },
+  {
+    "revision": "d89574cfb2df0918fc77",
+    "url": "/rainbow-piano-singalong/static/css/2.7e31b649.chunk.css"
+  },
+  {
+    "revision": "f1ed8a1cb45f2c88b52f",
+    "url": "/rainbow-piano-singalong/static/css/main.68b52a80.chunk.css"
+  },
+  {
+    "revision": "d89574cfb2df0918fc77",
+    "url": "/rainbow-piano-singalong/static/js/2.242262ed.chunk.js"
+  },
+  {
+    "revision": "e6001fb061ff520794147ccccd3e116e",
+    "url": "/rainbow-piano-singalong/static/js/2.242262ed.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f1ed8a1cb45f2c88b52f",
+    "url": "/rainbow-piano-singalong/static/js/main.886c9d31.chunk.js"
+  },
+  {
+    "revision": "72b0d6f94e626e7ecca2",
+    "url": "/rainbow-piano-singalong/static/js/runtime-main.091138e8.js"
+  },
+  {
+    "revision": "208a95f51f1eadead119458c57266161",
+    "url": "/rainbow-piano-singalong/static/media/TitleAbout.208a95f5.png"
+  },
+  {
+    "revision": "8bf32dff61fc5f8d733bf82638beed16",
+    "url": "/rainbow-piano-singalong/static/media/TitleContact.8bf32dff.png"
+  },
+  {
+    "revision": "fb5d9e390aab569807f3cfe0d0ca7d0b",
+    "url": "/rainbow-piano-singalong/static/media/TitlePricing.fb5d9e39.png"
+  }
+]);
