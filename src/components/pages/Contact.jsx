971db693d7bb8d7ccb4c668a6c865c0a63bbf361
sarahrainbow <sarah@thebatesons.com>
2021-03-29 16:14:04 +0100

    email copy to clipboard

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,20 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 import TextArea from '../TextArea';
 import Quote from '../Quote';
 import { Element } from 'react-scroll'
 import TitleContact from '../../images/TitleContact.png';
 import mailIcon from '../../images/MailIcon.png';
+import copyIcon from '../../images/copy.png';
 
-class Contact extends Component {
-    render(){
-        return(
-            <Element name="contact" id="Contact" className="section container-fluid">
-                <img src={TitleContact} className="title" alt="Title for contact section" />
-                <TextArea><img src={mailIcon} id="mailIcon" alt="mail icon"/><span id='contactEmail'>mark@thegreatrainbowpianosingalong.com</span></TextArea> 
-                <Quote>I didn’t realise I was alive before now</Quote>
-            </Element>
-        );
-    }
+const Contact = () => {
+  const email = "mark@thegreatrainbowpianosingalong.com";
+
+  const onCopy = () => {
+    navigator.clipboard.writeText(email)
+  }
+
+  return(
+      <Element name="contact" id="Contact" className="section container-fluid">
+          <img src={TitleContact} className="title" alt="Title for contact section" />
+          <TextArea>
+            <img src={mailIcon} id="mailIcon" alt="mail icon"/>
+            <span className='email'> 
+              <span id='contactEmail'>{email}</span>
+              <button className='copyButton' onClick={onCopy}>
+                <img src={copyIcon} alt="copy to clipboard" />
+              </button>
+            </span>
+          </TextArea> 
+          <Quote>I didn’t realise I was alive before now</Quote>
+      </Element>
+  );
 }
 
 export default Contact;
