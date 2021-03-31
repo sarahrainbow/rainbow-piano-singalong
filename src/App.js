b8a1c147af9df7c2a76669c82f67b8d8c997af03
sarahrainbow <sarah@thebatesons.com>
2021-03-31 08:55:49 +0100

    silent switch override

--- src/App.js
+++ src/App.js
@@ -8,6 +8,56 @@ import Pricing from './components/pages/Pricing'
 import Contact from './components/pages/Contact'
 
 function App() {
+
+  let isWebAudioUnlocked = false;
+let isHTMLAudioUnlocked = false;
+
+function overrideSilentSwitchMobile() {
+    if (isWebAudioUnlocked && isHTMLAudioUnlocked) return;
+
+    // Unlock WebAudio - create short silent buffer and play it
+    // This will allow us to play web audio at any time in the app
+    const context = new AudioContext();
+    const buffer = context.createBuffer(1, 1, 22050); // 1/10th of a second of silence
+    const source = context.createBufferSource();
+    source.buffer = buffer;
+    source.connect(context.destination);
+    source.onended = function () {
+        console.log('WebAudio unlocked!');
+        isWebAudioUnlocked = true;
+        if (isWebAudioUnlocked && isHTMLAudioUnlocked) {
+            console.log('WebAudio unlocked and playable w/ mute toggled on!');
+        }
+    };
+    source.start();
+
+    // Unlock HTML5 Audio - load a data url of short silence and play it
+    // This will allow us to play web audio when the mute toggle is on
+    const silenceDataURL = 'data:audio/wav;base64,UklGRjIAAABXQVZFZm10IBIAAAABAAEAQB8AAEAfAAABAAgAAABmYWN0BAAAAAAAAABkYXRhAAAAAA';
+    const tag = document.createElement('audio');
+    tag.controls = false;
+    tag.preload = 'auto';
+    tag.loop = false;
+    tag.src = silenceDataURL;
+    tag.onended = function () {
+        console.log('HTMLAudio unlocked!');
+        isHTMLAudioUnlocked = true;
+        if (isWebAudioUnlocked && isHTMLAudioUnlocked) {
+            console.log('WebAudio unlocked and playable w/ mute toggled on!');
+        }
+    };
+    const p = tag.play();
+    if (p) {
+        p.then(() => {
+            console.log('play success');
+        }, (reason) => {
+            console.log('play failed', reason);
+        });
+    }
+}
+
+overrideSilentSwitchMobile();
+
   return (
     <div className="App">
         <Header/>
